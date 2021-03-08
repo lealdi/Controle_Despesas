5a35a64b557db82e03ba266b5d6f2d301085c674
Diego Leal <diego_ldxsbc@hotmail.com>
2021-03-08 14:18:41 -0300

    start project

---
+++ script.js
@@ -0,0 +1,6 @@
+const dummyTransactions = [
+    {id: 1,name:"Bolo de brigadeiro",amount:-20},
+    {id: 2,name:"Salario",amount: 300},
+    {id: 3,name:"Torta de frango",amount: -10},
+    {id: 4,name:"Violao",amount: 150},
+]
